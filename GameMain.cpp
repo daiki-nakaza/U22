6548efb5f80feb8d7d3ab2dd89b70067e3a7e090
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2020-06-16 12:38:50 +0900

    敵の追加

--- GameMain.cpp
+++ GameMain.cpp
@@ -3,11 +3,15 @@
 
 #include "GameMain.h"
 #include "Map.h"
+#include "Enemy.h"
 
 
 
 void DrawGameMain() {
-	MapDisp();
-	MapMove();
+	MapDisp();			//マップの描画
+	MapMove();			//スクリーンの処理
+
+	enemyDisp();		//敵の描画処理
+	enemyMove();		//敵の処理
 }
 
