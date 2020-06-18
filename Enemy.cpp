43d127bfd22005603943afe45d4dc141f82e3ece
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2020-06-18 23:25:12 +0900

    敵の当たり判定の改善 敵の画像の追加

--- Enemy.cpp
+++ Enemy.cpp
@@ -4,6 +4,8 @@
 #include "Define.h"
 #include "Map.h"
 
+#define ENEMY_SIZE 48	//敵の大きさ
+
 /**************************************************
 *		変数の宣言
 ***************************************************/
@@ -16,14 +18,20 @@ enemyInfo g_Enemy;			//敵の情報を持った変数
 void enemyInfo::WalkInit() {                 // 敵の初期化
 	x = 24 * MAP_SIZE;							 // 敵のX座標の初期位置(マップチップの場所)
 	y = 15 * MAP_SIZE;								    // 敵のY座標の初期位置(マップチップの場所)
-	w = MAP_SIZE;						//敵の横幅
-	h = MAP_SIZE;						//敵の縦幅
+	w = ENEMY_SIZE;						//敵の横幅
+	h = ENEMY_SIZE;						//敵の縦
+
+	LoadDivGraph("images/MouseAll.png", 4, 4, 1, ENEMY_SIZE, ENEMY_SIZE, pic);
+	anm = 0;
 
 	direct = -1;						//左向きから始める
 
 	speed = 4;						//敵のスピード
+	picDir = true;
 
 	DispFlg = TRUE;					//敵を表示
+
+
 }
 
 void enemyInfo::Disp() {
@@ -34,6 +42,7 @@ void enemyInfo::Disp() {
 
 	if (DispFlg) {		//敵表示
 		DrawBox(x,y, x + w, y + h, 0x000000, true);
+		DrawRotaGraphFast2(x, y,0,0,1,0, pic[anm], true,picDir);
 	}
 	else {				//敵非表示
 
@@ -42,16 +51,25 @@ void enemyInfo::Disp() {
 
 void enemyInfo::WalkMove(){
 
+	static int AnmCnt = 0;
+	static int FrmMax = 15;
+
 	if (DispFlg) {
+		if (g_MapChip[(y / MAP_SIZE + MapY) + 1][(x / MAP_SIZE + MapX)]) {			//１つ下のマスを見て空中だったら
+			g_Enemy.y += GRAVITY;
+		}
+
 		if (EnemyCheckHit(g_Enemy)) {	//次のフレームの移動先を見て壁だったら
 			direct *= -1;			//移動の向きを反転させる
 		}
-
-		if (g_MapChip[(y / MAP_SIZE + MapY) + 1][(x / MAP_SIZE + MapX)]) {			//１つ下のマスを見て空中だったら
-			g_Enemy.y += 4;
-		}
-
+		if (direct < 0) { picDir = false; }
+		else { picDir = true; }
 		x += direct * speed;				//移動のスピードを敵キャラに入れる
+
+		if (++AnmCnt >= FrmMax) {
+			if (++anm > 3) anm = 0;
+			AnmCnt = 0;
+		}
 
 	}
 
@@ -81,20 +99,15 @@ void enemyMove() {
 bool EnemyCheckHit(enemyInfo enemy) {
 	if (enemy.direct == 1)			//右向きの処理
 	{
-		//if (g_MapChip[enemy.y / MAP_SIZE + MapY][(enemy.x / MAP_SIZE) + enemy.direct + MapX] == 0) return TRUE;
-		if (g_MapChip[(enemy.y + MapDrawPointY) / MAP_SIZE + MapY][((enemy.x - MapDrawPointX) / MAP_SIZE) + enemy.direct + MapX] == 0) return TRUE;
-	}else{							//左向きの処理
-		if (g_MapChip[(enemy.y + MapDrawPointY) / MAP_SIZE + MapY][( (enemy.x - MapDrawPointX + (enemy.direct * enemy.speed)) / MAP_SIZE) + MapX] == 0) return TRUE;
-	
-	/*if (g_MapChip[MapY + ((enemy.y + MapDrawPointY) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x +MapDrawPointX) / MAP_SIZE) - MapChipNumX] != 1
-		|| g_MapChip[MapY + ((enemy.y + MapDrawPointY + CHA_SIZE - 1) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x + MapDrawPointX) / MAP_SIZE) - MapChipNumX] != 1
-		|| g_MapChip[MapY + ((enemy.y + MapDrawPointY) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x + MapDrawPointX + CHA_SIZE - 1) / MAP_SIZE) - MapChipNumX] != 1
-		|| g_MapChip[MapY + ((enemy.y + MapDrawPointY + CHA_SIZE - 1) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x + MapDrawPointX + CHA_SIZE - 1) / MAP_SIZE) - MapChipNumX] != 1
-		|| g_MapChip[MapY + ((enemy.y + MapDrawPointY) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x + MapDrawPointX + CHA_SIZE / 2) / MAP_SIZE) - MapChipNumX ] != 1
-		|| g_MapChip[MapY + ((enemy.y + MapDrawPointY + CHA_SIZE - 1) / MAP_SIZE) + MapChipNumY][MapX + ((enemy.x + MapDrawPointX + CHA_SIZE / 2) / MAP_SIZE) - MapChipNumX] != 1
-		)
-	{*/
-		return false;
+		//次の移動の場所が敵キャラの右端に壁が重なったらスピードを反転する
+		if 
+			(g_MapChip[(enemy.y + MapDrawPointY) / MAP_SIZE + MapY]
+			[(((enemy.x + enemy.w - MapDrawPointX)+(enemy.direct * enemy.speed)) / MAP_SIZE) + MapX] == 0) return true;
+	}
+	else {							//左向きの処理
+		if
+			(g_MapChip[(enemy.y + MapDrawPointY) / MAP_SIZE + MapY]
+			[((enemy.x - MapDrawPointX + (enemy.direct * enemy.speed)) / MAP_SIZE) + MapX] == 0) return TRUE;
 	}
 	return false;
 }
--- Enemy.h
+++ Enemy.h
@@ -13,6 +13,10 @@ struct enemyInfo
 	int direct;			//敵の向き (右:1  左:-1)
 	int speed;			//敵のスピード
 
+	int pic[4];			//画像用変数
+	int anm;			//アニメーション用変数
+	bool picDir;			//画像の向き
+
 	bool DispFlg = FALSE;		//敵をの表示のフラグ
 
 
